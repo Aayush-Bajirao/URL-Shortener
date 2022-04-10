9ca94dd02b7415994d443f1a095a33ef1411c86c
Aayush Bajirao <aayubaji1208@gmail.com>
2022-04-10 12:35:11 +0530

    Changed Django ORM to SQL in migration folder in myapp

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,7 +1,4 @@
 from django.contrib import admin
-
-from URL_shortner.myapp.models import LongToShort
-
 from .models import LongToShort
 
 admin.site.register(LongToShort)
--- myapp/models.py
+++ myapp/models.py
@@ -1,12 +1,14 @@
-from datetime import datetime
-from enum import unique
-from unittest.util import _MAX_LENGTH
-from xml.dom.minidom import CharacterData
+from enum import auto
 from django.db import models
-from django.forms import CharField
 
 class LongToShort(models.Model):
     long_url = models.URLField(max_length = 5000)
-    short_url = CharacterData(max_length = 100, unique = True)
+    short_url = models.CharField(max_length = 100, unique = True)
     date = models.DateField(auto_now_add = True)
     clicks = models.IntegerField(default = 0)
+
+#After Changes in model.py DO THESE STEPS IMIDEITLY
+#1.Close server on cmd
+#2.pyhton manage.py makemigrations
+#3.python manage.py migrate
+#Model made are in python but we have to change it into SQL language
