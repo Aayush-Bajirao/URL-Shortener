d1745f987c653b6183eac1d2eb2165e34383d7c9
Aayush Bajirao <aayubaji1208@gmail.com>
2022-04-10 12:07:18 +0530

    Added database table with column names and registered in admin.py

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from URL_shortner.myapp.models import LongToShort
+
+from .models import LongToShort
+
+admin.site.register(LongToShort)
--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,12 @@
+from datetime import datetime
+from enum import unique
+from unittest.util import _MAX_LENGTH
+from xml.dom.minidom import CharacterData
 from django.db import models
+from django.forms import CharField
 
-# Create your models here.
+class LongToShort(models.Model):
+    long_url = models.URLField(max_length = 5000)
+    short_url = CharacterData(max_length = 100, unique = True)
+    date = models.DateField(auto_now_add = True)
+    clicks = models.IntegerField(default = 0)
